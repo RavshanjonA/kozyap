c08f8c8ff34e0a27342ca2da603bdf634e045a50
ravshanjon <akhmadjonovr.98@gmail.com>
2022-11-25 22:33:16 +0500

    todo list and instruments model

--- kozyap/models.py
+++ kozyap/models.py
@@ -1,13 +1,14 @@
+from django.core.exceptions import ValidationError
 from django.db import models
-from django.db.models import Model, AutoField, CharField, BigIntegerField
+
 from datetime import datetime, timedelta
-from django.db.models import CharField
 
 
 def get_date(*args, **kwargs):
     return datetime.strftime(datetime.now() - timedelta(1), '%Y-%m-%d')
 
-class Object(models.Model):
+
+class Object(models.Model):  # Obyekt
     name = models.CharField(verbose_name="Obyekt nomi", max_length=56)
     location = models.CharField(verbose_name="Joylashuvi ", max_length=56)
     geolokatsiyasi = models.CharField(verbose_name="Geolokatsiyasi", max_length=512)
@@ -21,11 +22,11 @@ class Object(models.Model):
         return self.name
 
 
-class Brigadier(Model):
-    full_name = CharField(verbose_name="Ism Familya", max_length=50)
-    tgid = BigIntegerField(verbose_name="Telegram ID", unique=True)
-    username = CharField(verbose_name="Telegram Username", max_length=20, null=True)
-    phone = CharField(max_length=13, null=True)
+class Brigadier(models.Model):  # Brigadir
+    full_name = models.CharField(verbose_name="Ism Familya", max_length=50)
+    tgid = models.BigIntegerField(verbose_name="Telegram ID", unique=True)
+    username = models.CharField(verbose_name="Telegram Username", max_length=20, null=True)
+    phone = models.CharField(max_length=13, null=True)
 
     class Meta:
         db_table = "bridgadier"
@@ -36,11 +37,11 @@ class Brigadier(Model):
         return self.full_name
 
     @property
-    def get_staff(self):
+    def staffs(self):
         return self.staff.all()
 
 
-class Staff(models.Model):
+class Staff(models.Model):  # Xodim
     full_name = models.CharField(verbose_name="Ism Familya", max_length=56)
     tgid = models.IntegerField(verbose_name="Telegram ID", unique=True, null=True, blank=True)
     username = models.CharField(verbose_name="Telegram Username", max_length=120, unique=True, null=True)
@@ -55,9 +56,12 @@ class Staff(models.Model):
     def __str__(self):
         return self.full_name
 
+    @property
+    def salary(self):
+        return self.reports.all()
 
-#
-class StaffReport(Model):
+
+class StaffReport(models.Model):  # Xodimlar Keldi Kettisi Bitta Hodim uchun
     date = models.DateField(verbose_name="Sana", editable=True)
     work_hour = models.DecimalField(verbose_name="Ish Smena", decimal_places=1, max_digits=3)
     stuff_id = models.ForeignKey(verbose_name="Hodim", to=Staff, on_delete=models.SET_DEFAULT, default=0,
@@ -72,7 +76,20 @@ class StaffReport(Model):
         verbose_name = "KunlikHodim"
         verbose_name_plural = "KunlikHodimlar"
 
-# class CABack(Model):
+
+class Intruments(models.Model):
+    owner = models.ForeignKey(verbose_name="Brigadier", to=Brigadier, on_delete=models.SET_DEFAULT, default=-1)
+    status = models.CharField(verbose_name="Holati", max_length=128)
+    name = models.CharField(verbose_name="Instrument Nomi", max_length=128)
+
+    class Meta:
+        db_table = "instrument"
+        verbose_name = "Uskuna"
+        verbose_name_plural = "Uskunalar"
+    def __str__(self):
+        return  self.name
+
+# class CABack(Model): #Keldi Ketdi
 #     brigadier_id = models.ForeignKey(verbose_name="Brigadir", to=Brigadier, on_delete=models.SET_NULL)
 #     object = models.ForeignKey(verbose_name="Obyekt", to=Object, on_delete=models.SET_NULL, max_length=512)
 #     location_name = models.CharField(verbose_name="Lokatsiya", max_length=512, on_delete=models.SET_NULL, null=True)
@@ -83,16 +100,54 @@ class StaffReport(Model):
 #     twage = models.DecimalField(verbose_name="Jami Kunlik Ish Haqi", decimal_places=2)
 #
 #     class Meta:
-#         db_table = "caback"
+#         db_table = "cback"
 #         verbose_name = "Keldi Ketdi"
 #
 #     def __str__(self):
 #         return self.brigadier_id.__str__()
 #
 #
-# class ToDoList(Model):
-#     date = models.DateField(verbose_name="Sana", value=get_date(), unique=False)
-#     brigadier = models.ForeignKey(verbose_name="Brigadir", Brigadier, on_delete=models.SET_NULL)
-#     object = models.ForeignKey(Object, on_delete=models.SET_NULL)
-#     stuff_count = models.IntegerField(null=False)
-#     todos = models.TextField()
+class ToDoList(models.Model):  # Qilingan IShlar Royhati
+    date = models.DateField(verbose_name="Sana", unique=False)
+    brigadier = models.ForeignKey(verbose_name="Brigadir", to=Brigadier, on_delete=models.SET_DEFAULT, default=-1)
+    object = models.ForeignKey(verbose_name="Obyekt", to=Object, on_delete=models.SET_DEFAULT, default=-1)
+    stuff_count = models.IntegerField(verbose_name="Hodimlar Soni", null=False)
+    todos = models.TextField(verbose_name="Qilingan Ishlar")
+    comment = models.TextField(verbose_name="Izoh", null=True)
+    measure = models.CharField(max_length=56, verbose_name="O'lchov birligi", null=True)
+    size = models.CharField(max_length=128, verbose_name="Qilingan ish obyomi", null=True)
+    price = models.IntegerField(verbose_name="Narxi", null=True)
+    total = models.IntegerField(verbose_name="Jami", null=True)
+    admin_comment = models.TextField(verbose_name="Admin Izohi", null=True)
+
+    class Meta:
+        db_table = "todos"
+        verbose_name = "Qilinga Ish Qo'shish"
+        verbose_name_plural = "Qilingan Ishlar Ro'yhati"
+
+    def __str__(self):
+        return  f"{self.object}"
+#
+#
+# class Cost(models.Model):  # Pul Harajatlari
+#     date = models.DateField()
+#     owner = models.CharField(verbose_name="Kim Berdi", null=False)
+#     stuff = models.CharField(verbose_name="Kim Oldi", null=False)
+#     object = models.ForeignKey(verbose_name="Qaysi obyekt uchun harajat", to=Object, related_name="costs",
+#                                on_delete=models.SET_DEFAULT, default=-1)
+#     reason = models.CharField(verbose_name="Maqsadi", )
+#     input = models.CharField(verbose_name="Kirim", null=True)
+#     output = models.CharField(verbose_name="Chiqim", null=True)
+#     balance = models.CharField(verbose_name="Qolgan Summa", null=False)
+#     admin_comment = models.TextField(verbose_name="Admin Izoh", null=False)
+#     def clean(self):
+#         if self.input and self.output:
+#             raise ValidationError(
+#                 "Iltimos Kirim Chiqimni bittasini tanlang")
+#         elif self.input and self.output:
+#             raise ValidationError(
+#                 "Iltimos Yoki Kirim ni Yoki CHiqimni toldiring")
+#
+# class Instrument(models.Model): #Instrument  Oldi berdisi
+#     owner = models.ForeignKey(verbose_name="Kim berdi",to=Brigadier, on_delete=models.SET_DEFAULT, default=-1)
+#     object = models.ForeignKey(verbose_name="Obyekt nomi",to=Object, on_delete=models.SET_DEFAULT, default=-1)
